363c635fa2007bbdb2765efcd9ce03830562e091
Dachi davituri <davituridachi33@gmail.com>
2025-01-13 14:14:26 +0400

    add carousel on about

--- src/pages/about/components/services/services.tsx
+++ src/pages/about/components/services/services.tsx
@@ -1,10 +1,17 @@
 import { servicesData } from "@/data";
 import { useTranslation } from "react-i18next";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 const Services: React.FC = () => {
   const { t } = useTranslation();
   return (
-    <div className="py-12">
+    <div className="p-3">
       <div className="container mx-auto px-6 text-center">
         <h2 className="mb-6 text-3xl font-semibold text-gray-800 dark:text-white">
           {t("about.services.title")}
@@ -12,19 +19,25 @@ const Services: React.FC = () => {
         <p className="mb-12 text-gray-600 dark:text-white">
           {t("about.services.subtitle")}
         </p>
-        <div className="grid gap-8 md:grid-cols-3">
-          {servicesData.map((service) => (
-            <div
-              key={service.key}
-              className="rounded-lg bg-white p-6 shadow-2xl dark:bg-gray-300"
-            >
-              <h3 className="mb-2 text-xl font-medium text-gray-800">
-                {t(service.titleKey)}
-              </h3>
-              <p className="text-gray-600">{t(service.descriptionKey)}</p>
-            </div>
-          ))}
-        </div>
+        <Carousel className="m-auto max-w-[85%]">
+          <CarouselContent>
+            {servicesData.map((service, index) => (
+              <CarouselItem key={index}>
+                <div
+                  key={service.key}
+                  className="h-full rounded-lg bg-gray-200 p-6 shadow-2xl dark:bg-gray-300"
+                >
+                  <h3 className="mb-2 text-xl font-medium text-gray-800">
+                    {t(service.titleKey)}
+                  </h3>
+                  <p className="text-gray-600">{t(service.descriptionKey)}</p>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
       </div>
     </div>
   );
